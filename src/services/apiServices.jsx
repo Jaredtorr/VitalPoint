a3e34e553d938fb54005f146bd63865f26dc333c
christianramirez <cm2849436@gmail.com>
2025-07-20 21:36:21 -0600

    Se agregó el nuevio servicio para poder obtener los datos de estrés

--- src/services/apiServices.jsx
+++ src/services/apiServices.jsx
@@ -65,3 +65,24 @@ export async function fetchpH(setData) {
         setData([]);
     }
 }
+
+export async function fetchStress(setData) {
+    try {
+        const response = await fetch(`${BASE_API_URL}/stress`);
+        if (!response.ok) {
+            throw new Error('Error al obtener los datos de estrés');
+        }
+
+        const data = await response.json();
+        const formattedData = data.map(item => ({
+            id: item.id,
+            esp32_id: item.esp32ID,
+            tiempo: item.tiempo,
+            estres: item.estres,
+        }));
+        setData(formattedData);
+    } catch (error) {
+        console.error('Error al conectar con la API de estrés:', error);
+        setData([]);
+    }
+}
